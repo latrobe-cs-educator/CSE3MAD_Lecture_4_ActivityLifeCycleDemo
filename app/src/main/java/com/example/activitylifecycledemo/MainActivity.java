588e9e14b9947a7f9426624800c6c87c792bb957
Marita Fitzgerald <maritafitzgerald@gmail.com>
2021-08-05 05:06:03 +1000

    Add Log messages

--- app/src/main/java/com/example/activitylifecycledemo/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycledemo/MainActivity.java
@@ -3,46 +3,58 @@ package com.example.activitylifecycledemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), "onCreate Called", Toast.LENGTH_LONG).show();
+        Log.d("Lifecycle Callback:", "onCreate Called" );
     }
 
+    @Override
     protected void onStart() {
         super.onStart();
         Toast.makeText(getApplicationContext(), "onStart Called", Toast.LENGTH_LONG).show();
+        Log.d("Lifecycle Callback:", "onStart Called" );
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Toast.makeText(getApplicationContext(), "onRestart Called", Toast.LENGTH_LONG).show();
+        Log.d("Lifecycle Callback:", "onRestart Called" );
     }
 
+    @Override
     protected void onPause() {
         super.onPause();
         Toast.makeText(getApplicationContext(), "onPause Called", Toast.LENGTH_LONG).show();
+        Log.d("Lifecycle Callback:", "onPause Called" );
     }
 
+    @Override
     protected void onResume() {
         super.onResume();
         Toast.makeText(getApplicationContext(), "onResume Called", Toast.LENGTH_LONG).show();
+        Log.d("Lifecycle Callback:", "onResume Called" );
     }
 
+    @Override
     protected void onStop() {
         super.onStop();
         Toast.makeText(getApplicationContext(), "onStop Called", Toast.LENGTH_LONG).show();
+        Log.d("Lifecycle Callback:", "onStop Called" );
     }
 
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         Toast.makeText(getApplicationContext(), "onDestroy Called", Toast.LENGTH_LONG).show();
+        Log.d("Lifecycle Callback:", "onDestroy Called" );
     }
 }
